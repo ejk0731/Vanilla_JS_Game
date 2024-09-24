4af681cebfc85c02efb42c8b869d296c4ae6675f
eunji <103646256+ejk0731@users.noreply.github.com>
2024-09-24 14:23:57 +0900

    feature: object, array

--- js/class.js
+++ js/class.js
@@ -1,65 +1,43 @@
 class Hero {
   constructor(el) {
-    // 넘어온 클래스 네임으로 검색해 변수에 담아줌
     this.el = document.querySelector(el);
-    // console.log(this.el);
-
-    // 히어로가 이동할 거리/ 스피드
     this.movex = 0;
     this.speed = 16;
-
-    // 엘리먼트의 크기/위치값을 알 수 있음
-    // console.log(this.el.getBoundingClientRect());
-
-    // 화면 높이값
-    // console.log(window.innerHeight)
-    // 히어로의 top값
-    // console.log(this.el.getBoundingClientRect().top);
-    // 화면 아래를 기준으로 한 히어로의 머리 위치
-    // console.log(window.innerHeight - this.el.getBoundingClientRect().top);
+    // 히어로가 어느쪽을 보고있는지 변수 설정
+    this.direction = 'right';
   }
-  // 히어로의 모션을 변경
+
   keyMotion() {
     if(key.keyDown['left']) {
+      this.direction = 'left';
       this.el.classList.add('run');
       this.el.classList.add('flip');
-      // console.log(this.position().right);
-
       this.movex = this.movex - this.speed;
     } else if(key.keyDown['right']) {
+      this.direction = 'right';
       this.el.classList.add('run');
       this.el.classList.remove('flip');
-      // console.log(this.position().right);
-
       this.movex = this.movex + this.speed;
     }
-    // console.log(this.movex)
-
     if(key.keyDown['attack']){
-      this.el.classList.add('attack');
-      // 공격키를 눌렀을때 인스턴스 생성
-      new Bullet();
+      if(!bulletComProp.launch) {
+        this.el.classList.add('attack');
+        bulletComProp.arr.push(new Bullet());
+        bulletComProp.launch = true;
+        // console.log(bulletComProp.arr.length);
+      }
+
     }
-
-
     if(!key.keyDown['left'] && !key.keyDown['right']) {
       this.el.classList.remove('run');
     }
-
     if(!key.keyDown['attack']){
       this.el.classList.remove('attack');
+      bulletComProp.launch = false; 
     }
-
-    // 키눌림의 딜레이 차이 때문에 움직임이 이상하게 보임
     this.el.parentNode.style.transform = `translateX(${this.movex}px)`
   }
 
-  // 자주 사용하는 값은 공통 처리!! 
-  // 화면 아래를 기준으로 top값을 설정함
-  // 화면 높이 - 히어로의 top값
-  // bottom : 히어로의 top값 - 히어로의 높이값(아래를 기준으로한 bottom 위치)
-  // 위치값을 구할때 위 또는 아래에 기준을 두지 않으면 화면의 크기가 달라질때 위치값이 맞지 않는 상황이 생길 수 있음
-  // 해상도가 달라지지 않는다면 상관없지만 웹에서 화면 크기가 변경될경우 꼭 기준을 잡아야함
   position() {
     return{
       left: this.el.getBoundingClientRect().left,
@@ -69,7 +47,6 @@ class Hero {
     }
   }
 
-  // getBoundingClientRect() 말고 다른방법으로~!
   size() {
     return {
       width: this.el.offsetWidth,
@@ -80,22 +57,55 @@ class Hero {
 
 class Bullet {
   constructor() {
-    // 수리검 클래스를 적용할 부모 노드 적기
     this.parentNode = document.querySelector('.game');
     this.el = document.createElement('div');
     this.el.className = 'hero_bullet';
     this.x = 0;
     this.y = 0;
+    this.speed = 30;
+    this.distance = 0;
+    this.bulletDirection = 'right';
     this.init();
   }
+
   init() {
-    // console.log(hero.size())
+    // 수리검이 생성될때 히어로의 방향값을 수리검의 방향값으로 넣어주기
+    this.bulletDirection = hero.direction === 'left' ? 'left' : 'right';
     this.x = hero.position().left + hero.size().width / 2;
     this.y = hero.position().bottom - hero.size().height / 2;
-    // 수리검은 위치값을 transform으로 잡기때문에 y좌표는 bottom기준으로 값을 빼줘야함
-    // console.log('x:'+ this.x);
-    // console.log('y:'+ this.y);
+    this.distance = this.x;
     this.el.style.transform = `translate(${this.x}px, ${this.y}px)`;
     this.parentNode.appendChild(this.el);
   }
+
+  moveBullet() {
+    // 반대 방향으로 가기 (반대방향으로 가면서 + 사진도 반대로 돌려주기)
+    let setRotate = '';
+    if(this.bulletDirection == 'left') {
+      this.distance -= this.speed;
+      setRotate = 'rotate(180deg)';
+    } else {
+      this.distance += this.speed;
+    }
+    this.el.style.transform = `translate(${this.distance}px, ${this.y}px) ${setRotate}`;
+    this.crashBullet();
+  }
+
+  position() {
+    return{
+      left: this.el.getBoundingClientRect().left,
+      right: this.el.getBoundingClientRect().right,
+      top: gameProp.screenHeight - this.el.getBoundingClientRect().top,
+      bottom: gameProp.screenHeight - this.el.getBoundingClientRect().top - this.el.getBoundingClientRect().height
+    }
+  } 
+
+  // 수리검이 화면 밖을 나가면 지워주기
+  crashBullet() {
+    if(this.position().left > gameProp.screenWidth || this.position().right < 0) {
+      this.el.remove();
+    }
+  }
+
+  
 }
--- js/game.js
+++ js/game.js
@@ -1,15 +1,18 @@
 /* eslint-disable */
-
-// 키보드 세팅하기
 const key = {
   keyDown : {},
   keyValue : {
     37: "left",
     39: "right",
     88: 'attack'
-    // 39: "up",
-    // 40: "down"
   }
+}
+
+// 수리검의 오브젝트와 배열 생성
+// 수리검 배열 attack키를 누를때 생성되는 수리검의 모든 인스턴스 저장 
+const bulletComProp = {
+  launch: false,
+  arr: []
 }
 
 const gameProp = {
@@ -19,44 +22,41 @@ const gameProp = {
 
 const renderGame = () => {
   hero.keyMotion();
-  // 재귀호출
+  bulletComProp.arr.forEach((arr, i)=> {
+    arr.moveBullet();
+  })
   window.requestAnimationFrame(renderGame);
-  // 무한반복됨! 초당 약 60 FPS(프레임)으로 호출 
-  // 키 값의 상태를 체크하고 히어로의 움직에 필요한 값을 변경해주기 때문에 키눌림 딜레이 없이 부드럽게 움직임
 }
 
 const windowEvent = () => {
   window.addEventListener('keydown', (e) => {
     key.keyDown[key.keyValue[e.which]] = true;
-    // console.log(key.keyDown);
-    // console.log(e.which);
-    // 누르면 true; 떼면 false;만들어주기 위해 데이터 넘겨줄라구~~
   });
   window.addEventListener('keyup', (e) => {
-    // console.log(e.key)
     key.keyDown[key.keyValue[e.which]] = false;
-    // console.log(key.keyDown);
-    // 왼쪽, 오른쪽 아니면 undefined 가 나오게 됨
   });
+  // 자주 쓰이는 것들은 공통으로 관리!! 
+  window.addEventListener('resize', e => {
+    gameProp.screenWidth = window.innerWidth;
+    gameProp.screenHeight = window.innerHeight;
+  })
 }
 
-// 클래스가 추가될 때 이미지 로드되는 현상 없애기
 const loadImg = () => {
-  const preLoadImageSrc = ['../../lib/images/ninja_attack.png', '../../lib/images/ninja_run.png'];
+  const preLoadImageSrc = ['../../lib/images/ninja_attack.png', ['../../lib/images/ninja_run.png']];
   preLoadImageSrc.forEach(arr => {
     const img = new Image();
     img.src = arr;
   })
 }
 
-let hero; // 인스턴스 생성
-// 키 누를 때 키모션 메소드를 호출!
+let hero; 
+
 const init = () => {
   hero = new Hero('.hero');
   loadImg();
   windowEvent();
   renderGame();
-  // console.log(hero.position())
 }
 
 window.onload = () => {
